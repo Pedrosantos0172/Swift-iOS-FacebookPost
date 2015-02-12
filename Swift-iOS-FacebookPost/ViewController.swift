b724b6a65bbae589e6545f95379bea525dcd65d6
rudiluis <rudi121_kemo@hotmail.com>
2015-02-12 13:37:58 +0000

    New things

--- Swift-iOS-FacebookPost/ViewController.swift
+++ Swift-iOS-FacebookPost/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
     
@@ -22,7 +23,44 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPost(sender: UIButton) {
-        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeFacebook) {
+            var facebookSheet = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            
+            facebookSheet.setInitialText(textView.text)
+            facebookSheet.addImage(imageView?.image)
+            
+            self.presentViewController(facebookSheet, animated: true, completion: nil)
+        } else {
+            var alert = UIAlertController(title: "Information", message: "Nenhuma conta do Facebook activa! Por favor conectar-se ao Facebook em Definições -> Facebook e tentar novamente!", preferredStyle: UIAlertControllerStyle.Alert)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+            alert.addAction(UIAlertAction(title: "Fechar", style: .Default, handler: { action in
+                switch action.style{
+                case .Default:
+                    print("fechei")
+                    
+                case .Cancel:
+                    println("cancel")
+                    
+                case .Destructive:
+                    println("destructive")
+                }
+            }))
+            alert.addAction(UIAlertAction(title: "Help", style: .Default, handler: { action in
+                switch action.style{
+                case .Default:
+                    let url : NSURL = NSURL(string: "http://appmobiles.paroquiasvitoria-amaro.com/ajuda_configurar_facebook.php")!
+                    UIApplication.sharedApplication().openURL(url);
+                    
+                case .Cancel:
+                    println("cancel")
+                    
+                case .Destructive:
+                    println("destructive")
+                }
+            }))
+        }
+
     }
     
     override func didReceiveMemoryWarning() {
