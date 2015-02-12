a29a97079d8b9de60821e0ed324a335c6a4d928c
rudiluis <rudi121_kemo@hotmail.com>
2015-02-12 13:32:59 +0000

    Connect things at ViewController

--- Swift-iOS-FacebookPost/ViewController.swift
+++ Swift-iOS-FacebookPost/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
 
+    @IBOutlet weak var textView: UITextField!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonPost(sender: UIButton) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
